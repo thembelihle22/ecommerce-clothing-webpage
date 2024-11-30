a76d3bea614e05d7c8268bd5831c09cf9c0dc2cb
thembelihle22 <thembelihlegasa22@gmail.com>
2024-11-30 13:35:41 +0200

    added social medis icons

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-alert("ello");
+function showForm(event) {
+  event.preventDefault();
+  return prompt("What is your name?");
+}
+
+let form = document.querySelector("#navBtn");
+addEventListener(click, "ShowForm");
