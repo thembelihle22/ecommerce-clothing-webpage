215b2d6fecd8fe9da84998c0fecf9f22cee33a99
thembelihle22 <thembelihlegasa22@gmail.com>
2024-12-01 00:03:34 +0200

    installed scrollreveal, added scroll animations

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-function showForm(event) {
-  event.preventDefault();
-  return prompt("What is your name?");
-}
-
-let form = document.querySelector("#navBtn");
-addEventListener(click, "ShowForm");
+ScrollReveal({
+  reset: true,
+  distance: "60px",
+  duration: 2500,
+  delay: 400,
+});
+ScrollReveal().reveal(".storeName, .fourth-headline", {
+  delay: 350,
+  origin: "left",
+});
+ScrollReveal().reveal(".icon, .seller-images, .indexBtn", { delay: 200 });
